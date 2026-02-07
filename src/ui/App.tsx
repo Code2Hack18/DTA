83c93703147dee668bdce312b4471f6c6ae88901
utkarshOjha2001 <utkarsh.ojha@audaxlabs.com>
2026-02-08 03:11:03 +0530

    Updates on onload message.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,9 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-     Hii utkarsh
-     oits a dev platform 
-     <div className="c" style={{border:"1px solid red", color:"yellow"}}>Welcome</div>
+       Welcome to Electron.
     </>
   )
 }
