fd15218f07573938c9b9a03ce410fd509fe1079d
utkarshOjha2001 <utkarsh.ojha@audaxlabs.com>
2026-02-08 03:08:37 +0530

    Electron-Setup

---
+++ src/ui/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     Hii utkarsh
+     oits a dev platform 
+     <div className="c" style={{border:"1px solid red", color:"yellow"}}>Welcome</div>
+    </>
+  )
+}
+
+export default App
